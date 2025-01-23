9fcc7c740e432152ec258d888f06a90edef6dce7
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-01-23 14:12:20 +0100

    Function Components: uso

--- react-19-curso/src/App.jsx
+++ react-19-curso/src/App.jsx
@@ -1,21 +1,14 @@
 
 import './App.css'
+import FunctionalComponent from './componentes/functionalComponent';
 
 function App() {
-
-  const functionWithParameters = ( parameter) => {
-    console.log(`Función llamada desde ${parameter}`);
-  }
-
-
-
   return (
     <>
       <h1 >React 19 Curso</h1>
-      <button onClick={() => functionWithParameters('Click')}>Click</button>
-      <button onMouseOver={() => functionWithParameters('MouseOver')}>Mouse Over</button>
-
-
+      <FunctionalComponent />
+      <FunctionalComponent />
+      <FunctionalComponent />
     </>
   )
 }
