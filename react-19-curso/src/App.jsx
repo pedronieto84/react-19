df5feb149bdaccbd7d38a9ac1f3483ac9039dd00
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-01-23 13:29:25 +0100

    Interpolar javascript dentro de HTML

--- react-19-curso/src/App.jsx
+++ react-19-curso/src/App.jsx
@@ -1,9 +1,23 @@
+/* eslint-disable no-constant-condition */
 import './App.css'
+
+// Puedo crear funciones como si fuera un archivo de javascript normal
+
+function sumar(){
+return 2+2
+}
+
+const nombre= 'Juan'
 
 function App() {
   return (
     <>
       <h1 className='color'>React 19 Curso</h1>
+
+      <h1>Funcion Sumar: { sumar() }</h1>
+      <h1>Operaciones Matemáticas: {3+2}</h1>
+      <h1>Trabajar con strings {`Hola ${nombre}`}</h1>
+      <h1>Aplicar lógica { ( 3+6 ) >4 ? 'mayor': 'menor'}</h1>
     </>
   )
 }
