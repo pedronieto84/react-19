3a9e5172bd754c4b7d427e5a1f3ab41b002a7b46
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-01-23 14:08:33 +0100

    Componente funcional: creación

---
+++ react-19-curso/src/componentes/functionalComponent.jsx
@@ -0,0 +1,11 @@
+// Se le llama functional component porque es una función
+
+const functionalComponent = () => {
+    return (
+        <>
+            <h1>Functional Component</h1>
+        </>
+    )
+}
+
+export default functionalComponent;
