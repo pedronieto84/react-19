4f5440ef6119f1cf948ba29391cca43ce00f2d2e
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-01-29 16:28:29 +0100

    Creo listado de titulos en HomePage

--- react-19-curso/src/pages/homePage.jsx
+++ react-19-curso/src/pages/homePage.jsx
@@ -1,8 +1,23 @@
 
+
+import data from "../mock-data/mock-data"
+
 export default function HomePage() {
-  return (
-    <div className="card">
-      <h1>Home Page</h1>
-    </div>
-  )
+
+    const mockData = data
+
+    console.log(mockData);
+    return (
+        <div className="d-flex justify-content-center align-items-center" >
+            <div className="container">
+                <ul className="list-group">
+                    {
+                        mockData.map((item, index) => (
+                            <li key={index} className="list-group-item">{item.title}</li>
+                        ))
+                    }
+                </ul>
+            </div>
+        </div>
+    )
 }
