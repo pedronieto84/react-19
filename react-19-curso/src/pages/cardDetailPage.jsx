02f511e537a44f51c7d76d26c83ee7c6fb472ca6
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-01-29 17:23:45 +0100

    Cargo el objeto del array en cardDetailPage y lo paso como props al cardDetailComponent HIjo.

--- react-19-curso/src/pages/cardDetailPage.jsx
+++ react-19-curso/src/pages/cardDetailPage.jsx
@@ -1,10 +1,29 @@
 import {useParams} from 'react-router-dom';
+import data from '../mock-data/mock-data.js';
+import CardDetailComponent from '../components/cardDetailComponent';
+
 
 export default function CardDetailPage() {
-    const {id} = useParams();
+    const { id } = useParams();
+
+    // Cargo el doc con el id que me llega por parámetro
+    // En realidad aquí se debería hacer una llamada a la API correspondiente
+    // O cargar la data de otra forma, pero para simplificar, lo hago así
+
+    const myDoc = data.find( ( each ) => {
+         return each.id === parseInt(id)
+    })
+    console.log('myDOc', myDoc);
+
     return (
-      <div>
-        <h1>Detail Page {id}</h1>
-      </div>
+        <>
+        <CardDetailComponent 
+         title={ myDoc.title }
+         id={ myDoc.id }
+         tags={ myDoc.tags }
+         info={ myDoc.info }
+         img={ myDoc.img }
+         />
+      </>
     )
   }
