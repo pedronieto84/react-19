72040cdd54ef02755ef7695ece2034a00947ca4d
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-01-29 15:29:22 +0100

    Preparo el enrutado dinámico básico.

--- react-19-curso/src/App.jsx
+++ react-19-curso/src/App.jsx
@@ -1,14 +1,36 @@
-
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import HomePage from './pages/homePage'
+import DetailPage from './pages/cardDetailPage'
 import './App.css'
 
 function App() {
-
+ 
+  const id = 1
 
   return (
     <>
-      <div>
-        hola
-      </div>
+      <Router>
+        <nav>
+          <ul>
+
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to={`/detail/${id}`}>Detalle</Link>
+            </li>
+           
+          </ul>
+        </nav>
+
+
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/detail/:id" element={<DetailPage />} />
+
+        </Routes>
+
+      </Router>
     </>
   )
 }
--- react-19-curso/src/pages/cardDetailPage.jsx
+++ react-19-curso/src/pages/cardDetailPage.jsx
@@ -1,7 +1,10 @@
+import {useParams} from 'react-router-dom';
+
 export default function CardDetailPage() {
+    const {id} = useParams();
     return (
       <div>
-        <h1>Home Page</h1>
+        <h1>Detail Page {id}</h1>
       </div>
     )
   }
