31f96ade2be480eb8a90e1d43cca3062d8013e81
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-01-31 14:32:30 +0100

    Conecto el store a toda la aplicación con el Provider de react-redux.

--- react-19-curso/react-19-curso/src/App.tsx
+++ react-19-curso/react-19-curso/src/App.tsx
@@ -1,8 +1,8 @@
 
 import './App.css'
 import Counter from './components/Counter'
-//import { Provider } from "react-redux";
-//import store from './store/store';
+import { Provider } from "react-redux";
+import store from './store/store';
 import React from "react";
 
 function App() {
@@ -11,9 +11,9 @@ function App() {
   return (
     <>
       <React.StrictMode>
-        
+        <Provider store={store}>
           <Counter />
-    
+        </Provider>
       </React.StrictMode>
     </>
   )
