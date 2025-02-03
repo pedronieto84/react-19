7ad56bb1444af2301baf602031cbeefb929b9d76
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-02-03 23:04:04 +0100

    Tipado de datos a variables/const.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
 
 
-// Parametros funcionale no pueden ir al principio
-const sumarConMensajeDefecto = (a: number, b: number): ( number | string) => {
-    return a+b +3
-}
-console.log(sumarConMensajeDefecto(1, 2)) // 3;
+// Tipado de datos básico
+
+let nombre: string = "Juan";
+let edad: number = 25;
+let activo: boolean = true;
+
+// Tipado de datos especiales
+let desconocido: unknown = "puede ser cualquier cosa" // Pero no me dejará acceder a propiedades de este tipo de dato
+const propiedadDeDesconocido = desconocido.propiedad // Error
+let nulo: null = null;
+let indefinido: undefined = undefined;
+let cualquierCosa: any = 42 //Digamos que deshabilita el tipado de Typescript y hace que opere como si fuera javascript
+
+// Or
+let nombreOpcional: string | undefined = undefined;
 
