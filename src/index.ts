726a20832e731644dc2af52b9effb2314697f338
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-02-04 00:01:15 +0100

    Genericos y ejemplo de uso con promesas

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,36 @@
-// Void es que no devuelve nada
-
-type SumaParameter = (a: number, b: number) => void; 
-const suma = (a:number,  b:number):void => {
+// Generics <T> en TypeScript se usa para trabajar con un dato genérico, es decir, se lo defino cuando uso la función o clase.
+function identidad<T>(valor: T): T {
+    return valor;
+  }
   
-  return a+b
-};
-
-// As me sirve para convertir un dato en otro tipo de dato
-
-let valor: any = "Hola, TypeScript";
+  let numero = identidad<number>(42);
+  let texto = identidad<string>("Hola");
 
 
-// Sintaxis con 'as'
-let longitud2: number = (valor as string).length; // Assertion - o Casting, le digo yo a Typescript que tipo de dato es
+  // Se usa sobretodo en Promsesas y Observables principalmente
+  // VEAMOS EJEMPLO
+  function fetchData<T>(url: string): Promise<T> {
+    return new Promise<T>((resolve, reject) => {
+      setTimeout(() => {
+        try {
+          const data = { mensaje: "Datos obtenidos", exito: true } as T;
+          resolve(data);
+        } catch (error) {
+          reject("Error al obtener los datos");
+        }
+      }, 1000);
+    });
+  }
+  
+  // Uso con un tipo específico, por ejemplo, se que tipo de dato voy a recibir del back
+  interface UserStatus {
+    email: string;
+    verified: boolean;
+  }
+  
+  // Autollamo la promesa y vemos como resultado ya ha inferido el tipo de dato UserStatus correctamente
+  (async ()=>{
+    const resultado =  await fetchData<UserStatus>("https://api.example.com/data")
+    console.log('resultado', resultado); })()
 
-
-console.log(longitud2); // 17
+    
