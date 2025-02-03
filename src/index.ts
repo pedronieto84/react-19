261b262c15aeab686c7d42e21d479661cd82c1e2
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-02-04 00:09:53 +0100

    Keyword "IN" con ejemplo de objeto sofisticado.

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,13 @@
-// Estructuras complejas de objetos
+// Veamos el uso de la keyword "IN" en otro ejemplo de objeto complejo
+type Estados = "pendiente" | "aprobado" | "rechazado";
 
-type Datos = {
-    [key: string]: string;
-  };
-  
-  const usuario: Datos = {
-    nombre: "Juan",
-    email: "juan@example.com",
-    ciudad: "Madrid",
-  };
-  
-// Cuando el key es un number
-type Datos2 = {
-    [key: number]: string;
-  };
-  
-  const usuario2: Datos2 = {
-    1: "Juan",
-    2: "Luis"
-}
+type Pedidos = {
+  [estado in Estados]: number;
+};
 
-// Cuando el key es un template literal
-type Datos3 = {
-    [key: `user_${number}`]: string;
-  };
-  
-  const usuario3: Datos3 = {
-    user_1: "Juan",
-    user_2: "Luis",
-    nombre: "Juan", // fijaros que esto da error
-  };
-
-  // Flexible
-  type Flexible = {
-    [key: string]: string | number | boolean | {nombre: string, edad: number};
-  };
-  
-  const config: Flexible = {
-    modoOscuro: true,
-    volumen: 80,
-    usuario: "Alice",
-    datos: {
-      nombre: "Alice",
-      edad: 30,
-    },
-  };
+const pedidos: Pedidos = {
+  pendiente: 10,
+  aprobado: 5,
+  rechazado: 2,
+  australia: 2
+};
