d6411050d5cf9404507ba18b8fdb6d0d3b717824
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-02-03 23:16:15 +0100

    Cuando desconozco cuantos parametros vienen en una función.

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,13 @@
 
 
-// Objetos
+// FUnciones con numero indeterminado de parametros
 
-const persona: {nombre: string, edad: number, DNI: string} = {
-    nombre : 'Fernando',
-    edad: 30, // si quito la edad, me da error porque falta edad
-    DNI: '12345678Z'
-}
-
-// Objetos con parámetros opcionales
-const persona2: {nombre: string, edad: number, DNI?: string} = {
-    nombre : 'Fernando',
-    edad: 30
-    // DNI: '12345678Z' // si quito el DNI, no me da error porque es opcional
-}
-
-
+function registrarUsuario(nombre: string, ...roles: string[]) {
+    console.log(`Usuario: ${nombre}`);
+    console.log(`Roles: ${roles.join(", ")}`);
+  }
+  
+  registrarUsuario("Juan", "Admin", "Editor");
 
 
 
