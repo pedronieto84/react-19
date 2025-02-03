e317ebcbb1eb5732e58c89de23348355a9751f3b
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-02-03 23:52:40 +0100

    As y void

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
-enum Color {
-    Rojo = "Rojo",
-    Verde = "Verde",
-    Azul = "Azul",
-  }
-  
-  let colorFavorito: Color = Color.Azul;
+// Void es que no devuelve nada
 
+type SumaParameter = (a: number, b: number) => void; 
+const suma = (a:number,  b:number):void => {
+  
+  return a+b
+};
+
+// As me sirve para convertir un dato en otro tipo de dato
+
+let valor: any = "Hola, TypeScript";
+
+
+// Sintaxis con 'as'
+let longitud2: number = (valor as string).length; // Assertion - o Casting, le digo yo a Typescript que tipo de dato es
+
+
+console.log(longitud2); // 17
