eefef8eb24b2388a2bbbdb2a5c861ac0dd5cbd84
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-02-03 22:41:05 +0100

    Parametros funcionales no pueden ir al principio

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,7 @@
 
 
-// Definir parámetros en funciones
-const sumar = (a: number, b: number) :number=> {
-    return a + b
+// Parametros funcionale no pueden ir al principio
+const sumarConMensajeDefecto = (a?: number, b: number): ( number | string) => {
+    return ` ${a + b}`
 }
 
-// Definir parámetros opcionales en funciones
-const sumarConMensaje = (a: number, b: number, mostrarMensaje?: string): ( number | string) => {
-    if(mostrarMensaje){
-        return `${mostrarMensaje} ${a + b}`
-    }
-    return a + b
-}
-
-// Definir parámetros por defecto en funciones
-const sumarConMensajeDefecto = (a: number, b: number, mostrarMensaje: string = 'El resultado es: '): ( number | string) => {
-    return `${mostrarMensaje} ${a + b}`
-}
-
-
-
-
-console.log('sumar', sumar(1, 2));
-console.log('sumar mensaje', sumarConMensaje(1, 2, 'La suma es: '));
-console.log('sumar mensaje', sumarConMensajeDefecto(1, 2));
