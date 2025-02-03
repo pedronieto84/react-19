aab5d34006ce2f09cd2a13d67164da8da582ed65
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-02-03 22:58:29 +0100

    Añado un servidor de desarrollo nodemon y ts-node (para transpilar directamente). Creo archivo nodemon donde indico la info que necesita nodemon.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 
 
 // Parametros funcionale no pueden ir al principio
-const sumarConMensajeDefecto = (a?: number, b: number): ( number | string) => {
-    return ` ${a + b}`
+const sumarConMensajeDefecto = (a: number, b: number): ( number | string) => {
+    return a+b +3
 }
+console.log(sumarConMensajeDefecto(1, 2)) // 3;
 
