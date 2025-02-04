21d3efeb0c63aaa5b8dfb6687e41cb469e0159a7
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-02-04 17:47:02 +0100

    Misma funcionalidad pero para un componente de clase.

--- consultas-04-02/src/App.jsx
+++ consultas-04-02/src/App.jsx
@@ -1,55 +1,76 @@
-import { useState, useEffect, useRef } from "react";
+import { Component, createRef } from "react";
+import * as ReactLibrary from "react";
 
-import "./App.css";
+class App extends ReactLibrary.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      reload: 0,
+      viewportWidth: window.innerWidth,
+    };
+    this.divRef = createRef();
+  }
 
-function App() {
-  const [data, setData] = useState(0);
-  const [reload, setReload] = useState(0);
-  const divRef = useRef(null);
-  const [viewportWidth, setViewportWidth] = useState(window.innerWidth);
-  // Función para actualizar el ancho del viewport
-  const updateViewportWidth = () => {
-    const previousWidth = viewportWidth;
+  componentDidMount() {
+    console.log("did mount");
+    // Añado un event listener tradicional de javascript y le paso el evento handler
+    window.addEventListener("resize", this.updateViewportWidth);
+    this.fetchApi();
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.state.reload !== nextState.reload) {
+      console.log("should update", this.state.reload, nextState.reload);
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  componentWillUnmount() {
+    console.log("did unmount");
+    window.removeEventListener("resize", this.updateViewportWidth);
+  }
+
+  // Event handler
+  updateViewportWidth = () => {
+    console.log("updateViewportWidth");
     const newWidth = window.innerWidth;
-    const diffWidth = previousWidth - newWidth
-    const diffPoints = diffWidth > 0 ? diffWidth : diffWidth * -1
-    if(diffPoints > 100){ setReload(reload + 1) }
-    setViewportWidth(window.innerWidth);
+    const previousWidth = this.state.viewportWidth;
+    const diffWidth = Math.abs(previousWidth - newWidth);
+    console.log("diffWidth", diffWidth);
+    if (diffWidth > 100) {
+      console.log("more than 100", diffWidth);
+      this.setState((prevState) => ({ reload: prevState.reload + 1 }));
+      this.setState({ viewportWidth: newWidth });
+      this.fetchApi();
+    }
   };
 
-  const fetchApi = async () => {
-    console.log('FETCH');
+  fetchApi = async () => {
+    console.log("FETCH");
     const response = await fetch("https://jsonplaceholder.typicode.com/todos/");
     const data = await response.json();
-    setData(data);
+    this.setState({ data });
     console.log(data);
   };
 
-  useEffect(() => {
-
-        // Agregar el event listener al montar el componente
-        window.addEventListener('resize', updateViewportWidth);
-        
-        fetchApi()
-        // Limpiar el event listener al desmontar el componente equivalente al 
-        // ComponentWillUnmount aqui limpiaremos el event listener, timers...
-        return () => {
-          window.removeEventListener('resize', updateViewportWidth);
-        };
-    //fetchApi();
-  }, [reload]);
-
-  return (
-    <>
-      <div ref={divRef}>
-        <h1>Consultas 04-02</h1>
-        <h2>{viewportWidth}</h2>
-        <ul>
-          {data && data.map((item) => <li key={item.id}>{item.title}</li>)}
-        </ul>
-      </div>
-    </>
-  );
+  render() {
+    return (
+      <>
+        <div ref={this.divRef}>
+          <h1>Consultas 04-02</h1>
+          <h2>{this.state.viewportWidth}</h2>
+          <ul>
+            {this.state.data.map((item) => (
+              <li key={item.id}>{item.title}</li>
+            ))}
+          </ul>
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
