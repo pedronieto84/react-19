534a5bdb4abaac03302741e569f1394f8cc30a1e
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-02-04 19:06:44 +0100

    Añado diseño elemental de Container con navbar. (solamente estilos).

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,47 @@ function App() {
  
 
   return (
-    <>
-     <h1 className='card'>Hola mundo</h1>
-    </>
+    <div>
+    {/* Navbar de Bootstrap */}
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">Mi App</a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">Inicio</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Acerca de</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Servicios</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Contacto</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    {/* Contenido principal */}
+    <div className="container mt-4">
+      <h1>Bienvenido a Mi App</h1>
+      <p>Este es un ejemplo básico de una aplicación React con TypeScript y Bootstrap.</p>
+    </div>
+  </div>
   )
 }
 
