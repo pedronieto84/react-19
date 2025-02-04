38c7682933c0b1a2b22e7ca75f2f8ac00bf9f51d
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-02-04 21:08:14 +0100

    Preparo Primer Gráfico con Mock Data.

--- src/components/GraphsComponent.tsx
+++ src/components/GraphsComponent.tsx
@@ -13,11 +13,54 @@ import {
 import { Line } from 'react-chartjs-2';
 
 function GraphsComponent() {
+
+// Registro de los plugins de Chart.js
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend
+);
+
+// Opciones del gráfico
+ const options = {
+  responsive: true,
+  // plugins: {
+  //   legend: {
+  //     position: 'top' as const,
+  //   },
+  //   title: {
+  //     display: true,
+  //     text: 'Gráfico de linea',
+  //   },
+  // },
+};
+
+// Etiquetas para el eje X
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+ const data = {
+  labels,
+  datasets: [
+    {
+      fill: true, // Si rellena o no el area de abajo
+      label: 'Dataset 2', // Nombre del set
+      data: labels.map(() => Math.floor(Math.random() * 10) + 1),
+      borderColor: 'rgb(53, 162, 235)', // esto lo puedo quitar y escoge otro color por default
+      backgroundColor: 'rgba(53, 162, 235, 0.5)', // tambien puedo quitarlo o cambiarlo
+    },
+  ],
+};
  
 
     return (
       <>
-       <h1 className='card'>Graphs Component</h1>
+       <Line options={options} data={data} />; 
       </>
     )
   }
