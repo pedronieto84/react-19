1de9ac8f8de9be8095ea780d044af5c8cfbf4de9
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-02-04 17:07:54 +0100

    UseEffect para escuchar el cambio de tamaño de un div.

--- consultas-04-02/src/App.jsx
+++ consultas-04-02/src/App.jsx
@@ -1,9 +1,17 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 import "./App.css";
 
 function App() {
   const [data, setData] = useState(0);
+  const divRef = useRef(null);
+
+  const [viewportWidth, setViewportWidth] = useState(window.innerWidth);
+
+  // Función para actualizar el ancho del viewport
+  const updateViewportWidth = () => {
+    setViewportWidth(window.innerWidth);
+  };
 
   const fetchApi = async () => {
     const response = await fetch("https://jsonplaceholder.typicode.com/todos/");
@@ -13,18 +21,27 @@ function App() {
   };
 
   useEffect(() => {
-    fetchApi();
-  }, []);
+
+        // Agregar el event listener al montar el componente
+        window.addEventListener('resize', updateViewportWidth);
+        fetchApi()
+        // Limpiar el event listener al desmontar el componente equivalente al 
+        // ComponentWillUnmount aqui limpiaremos el event listener, timers...
+        return () => {
+          window.removeEventListener('resize', updateViewportWidth);
+        };
+    //fetchApi();
+  }, [viewportWidth]);
 
   return (
     <>
-      <h1>Consultas 04-02</h1>
-      <h2>Lista de tareas</h2>
-      <ul>
-        {data && data.map((item) => (
-          <li key={item.id}>{item.title}</li>
-        ))}
-      </ul>
+      <div ref={divRef}>
+        <h1>Consultas 04-02</h1>
+        <h2>{viewportWidth}</h2>
+        <ul>
+          {data && data.map((item) => <li key={item.id}>{item.title}</li>)}
+        </ul>
+      </div>
     </>
   );
 }
