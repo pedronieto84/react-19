df360eff92f7f0874a9cd1a2a8124cc4b997deca
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-02-04 21:25:57 +0100

    Añado el useEffect al GraphPage

--- src/pages/GraphsPage.tsx
+++ src/pages/GraphsPage.tsx
@@ -1,7 +1,35 @@
 import GraphsComponent from "../components/GraphsComponent"
+import {useState, useEffect} from "react";
+import { FlexibleObjectTable } from "./../types/types";
 
 function GraphsPage() {
  
+  // Data state donde almacenaré la response de la api
+  const [data, setData] = useState<FlexibleObjectTable[]>([]);
+
+  // Para gestionar el estado de loading y error
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+   // Fetch data from the Pokémon API
+   useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://pokeapi.co/api/v2/pokemon?limit=10"
+        );
+        setData(response.data.results); // Store the list of Pokémon
+        setLoading(false); // Set loading to false
+      } catch (error) {
+        const errorString = new Error(String(error)); // Convertir el error a un objeto `Error`
+        console.error(error);
+        setError(errorString.message); // Handle errors
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
 
     return (
       <>
