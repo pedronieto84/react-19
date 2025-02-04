e87a8925f52ecb8b59ce37c4dcc880a2b11a9f40
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-02-04 23:13:04 +0100

    Creo el enrutamiento para navegar a forms.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import FormsPage from './pages/FormsComponent';
 import GraphsPage from './pages/GraphsPage'
 import TablePage from './pages/TablePage'
 import { Navbar, Nav, Container } from "react-bootstrap";
@@ -18,6 +19,7 @@ function App() {
             <Nav className="me-auto">
               <Nav.Link as={Link} to="/table">Tablas</Nav.Link>
               <Nav.Link as={Link} to="/graphs">Gráficos</Nav.Link>
+              <Nav.Link as={Link} to="/forms">Formularios</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Navbar>
@@ -25,6 +27,7 @@ function App() {
           <Routes>
             <Route path="/table" element={<TablePage />} />
             <Route path="/graphs" element={<GraphsPage />} />
+            <Route path="/graphs" element={<FormsPage />} />
             <Route path="/" element={<h2>Escoge una opción</h2>} />
           </Routes>
         </div>
