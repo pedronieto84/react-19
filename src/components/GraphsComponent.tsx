2161a6f47d406c2eb8752ffc6cc8f23c84ecfe2f
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-02-04 20:58:33 +0100

    Importo al graphs component todos los React Elements que voy a necesitar.

--- src/components/GraphsComponent.tsx
+++ src/components/GraphsComponent.tsx
@@ -1,3 +1,16 @@
+import React from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
 
 function GraphsComponent() {
  
