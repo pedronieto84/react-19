57568ee42e7cdc85103fe8a7047ff76d522b08fd
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-02-04 20:04:07 +0100

    Paso el array de data por props al componente Tabla. Importo interfaces en Pagina y en Component, pongo los tipados y pueblo la tabla con el array.

--- src/components/TableComponent.tsx
+++ src/components/TableComponent.tsx
@@ -1,41 +1,31 @@
-import Table from 'react-bootstrap/Table'
+import Table from "react-bootstrap/Table";
+import { User } from "./../types/types";
 
-function TableComponent() {
- 
+function TableComponent({ data }: { data: User[] }) {
+  return (
+    <>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Age</th>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((person: User) => (
+            <tr key={person.id}>
+              <td>{person.id}</td>
+              <td>{person.name}</td>
+              <td>{person.age}</td>
+              <td>{person.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </>
+  );
+}
 
-    return (
-      <>
-         <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Username</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td colSpan={2}>Larry the Bird</td>
-          <td>@twitter</td>
-        </tr>
-      </tbody>
-    </Table>
-      </>
-    )
-  }
-  
-  export default TableComponent
+export default TableComponent;
--- src/pages/TablePage.tsx
+++ src/pages/TablePage.tsx
@@ -1,12 +1,20 @@
 import TableComponent from "../components/TableComponent"
+import { User } from './../types/types'
 
 function TablePage() {
+
+  const data: User[] = [
+    { id: 1, name: 'Juan Pérez', age: 25, email: 'juan@example.com' },
+    { id: 2, name: 'María Gómez', age: 30, email: 'maria@example.com' },
+    { id: 3, name: 'Carlos López', age: 28, email: 'carlos@example.com' },
+    { id: 4, name: 'Ana Martínez', age: 22, email: 'ana@example.com' },
+  ];
  
 
     return (
       <>
        <h1 className='card'>Tablas</h1>
-       <TableComponent />
+       <TableComponent data={data}/>
       </>
     )
   }
