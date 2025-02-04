bf72c0163f00f4b458255060b6385e9f99fce7c6
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-02-04 16:52:58 +0100

    Llamo a una api pública mediante useEffect y fetch. Y seteo un listado de items.

--- consultas-04-02/src/App.jsx
+++ consultas-04-02/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState(0);
+
+  const fetchApi = async () => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos/");
+    const data = await response.json();
+    setData(data);
+    console.log(data);
+  };
+
+  useEffect(() => {
+    fetchApi();
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Consultas 04-02</h1>
+      <h2>Lista de tareas</h2>
+      <ul>
+        {data && data.map((item) => (
+          <li key={item.id}>{item.title}</li>
+        ))}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
