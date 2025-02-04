695e50be991795727e4426b6c53659b33e722eb7
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-02-04 20:31:55 +0100

    Inicializo 3 estados, uno para el array de data, y 2 para error y loading.

--- src/pages/TablePage.tsx
+++ src/pages/TablePage.tsx
@@ -1,15 +1,22 @@
 import TableComponent from "../components/TableComponent";
 import { FlexibleObjectTable } from "./../types/types";
 import { getKeysFromArrayOfObjects }  from "../hooks/getKeysFromArrayOfObjects";
-import { useEffect, useState } from "react";
+import {  useState } from "react";
 
 function TablePage() {
-  const data: FlexibleObjectTable[] = [
-    { id: 1, name: "Juan Pérez", age: 25, email: "juan@example.com" },
-    { id: 2, name: "María Gómez", age: 30, email: "maria@example.com" },
-    { id: 3, name: "Carlos López", age: 28, email: "carlos@example.com" },
-    { id: 4, name: "Ana Martínez", age: 22, email: "ana@example.com" },
-  ];
+  // const data: FlexibleObjectTable[] = [
+  //   { id: 1, name: "Juan Pérez", age: 25, email: "juan@example.com" },
+  //   { id: 2, name: "María Gómez", age: 30, email: "maria@example.com" },
+  //   { id: 3, name: "Carlos López", age: 28, email: "carlos@example.com" },
+  //   { id: 4, name: "Ana Martínez", age: 22, email: "ana@example.com" },
+  // ];
+
+    // Data state donde almacenaré la response de la api
+    const [data, setData] = useState<FlexibleObjectTable[]>([]);
+
+    // Para gestionar el estado de loading y error
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string | null>(null);
 
   
   
