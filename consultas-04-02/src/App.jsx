84a0ee91e0d5911e4b446e22cb4c213dc6607345
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-02-04 17:16:23 +0100

    Añado optimización añadiendo un nuevo estado que controle el rerenderizado solamente cuando los puntos del width pasen de 100.

--- consultas-04-02/src/App.jsx
+++ consultas-04-02/src/App.jsx
@@ -4,16 +4,21 @@ import "./App.css";
 
 function App() {
   const [data, setData] = useState(0);
+  const [reload, setReload] = useState(0);
   const divRef = useRef(null);
-
   const [viewportWidth, setViewportWidth] = useState(window.innerWidth);
-
   // Función para actualizar el ancho del viewport
   const updateViewportWidth = () => {
+    const previousWidth = viewportWidth;
+    const newWidth = window.innerWidth;
+    const diffWidth = previousWidth - newWidth
+    const diffPoints = diffWidth > 0 ? diffWidth : diffWidth * -1
+    if(diffPoints > 100){ setReload(reload + 1) }
     setViewportWidth(window.innerWidth);
   };
 
   const fetchApi = async () => {
+    console.log('FETCH');
     const response = await fetch("https://jsonplaceholder.typicode.com/todos/");
     const data = await response.json();
     setData(data);
@@ -24,6 +29,7 @@ function App() {
 
         // Agregar el event listener al montar el componente
         window.addEventListener('resize', updateViewportWidth);
+        
         fetchApi()
         // Limpiar el event listener al desmontar el componente equivalente al 
         // ComponentWillUnmount aqui limpiaremos el event listener, timers...
@@ -31,7 +37,7 @@ function App() {
           window.removeEventListener('resize', updateViewportWidth);
         };
     //fetchApi();
-  }, [viewportWidth]);
+  }, [reload]);
 
   return (
     <>
