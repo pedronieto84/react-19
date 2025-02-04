22a64709ca1e93614d4f6f9286896a7109eb46a2
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-02-04 01:32:24 +0100

    Generación dinámica de Links desde un array.

--- react-19-curso/src/App.jsx
+++ react-19-curso/src/App.jsx
@@ -1,30 +1,35 @@
-import './App.css';
-import { BrowserRouter, Routes, Route, Link, useNavigate } from 'react-router-dom';
-import Dinamica from './paginas/dinamica';
+import "./App.css";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+  useNavigate,
+} from "react-router-dom";
+import Dinamica from "./paginas/dinamica";
 
 function App() {
-  const navigate = useNavigate();
-  
-  // Como esta función está fuera del "Browser"
-  const goToUserProfile = () => {
-    navigate('/dinamica/1');
-  };
+  // Array de números del 1 al 10 aquí podríamos poner todas las rutas que a las que queremos navegar
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   return (
-    <div>
-      
+    <>
       <nav>
         <ul>
-          <li>
-            <Link to="/dinamica/1/post/1?id=123&filter=active">Usuario 1 - Post 1</Link>
-          </li>
+          {/* Generar enlaces dinámicamente */} 
+          {numbers.map((number) => (
+            <li key={number}>
+              <Link to={`/dinamica/${number}`}>Número {number}</Link>
+            </li>
+          ))}
         </ul>
       </nav>
+
       <Routes>
-        <Route path="/dinamica/:userId/post/:postId" element={<Dinamica />} />
+        {/* Ruta dinámica para mostrar el número seleccionado */}
+        <Route path="/dinamica/:id" element={<Dinamica />} />
       </Routes>
-      
-    </div>
+    </>
   );
 }
 
--- react-19-curso/src/paginas/dinamica.jsx
+++ react-19-curso/src/paginas/dinamica.jsx
@@ -2,21 +2,13 @@ import { useParams, useSearchParams } from "react-router-dom";
 
 function Dinamica() {
 
-  const {userId} = useParams();
-  // Obtén los parámetros de la URL
-  const [searchParams] = useSearchParams();
-  // Esto me devuelve la string de query id=4&filter=3
-  console.log(searchParams);
+  const {id} = useParams();
 
-  // Si quiero obtener los valores de los query params (id y filter)
-  const idParams = searchParams.get("id"); 
-  const filter = searchParams.get("filter");
 
   return (
     <div>
-      <p>User ID: {userId}</p>
-      <p>Filter: {filter}</p>
-      <p>Id param: {idParams}</p>
+      <p>User ID: {id}</p>
+ 
     </div>
   );
 }
