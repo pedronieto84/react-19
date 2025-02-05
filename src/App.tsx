f04372164bb013c9f76553e2f54ecb55955bfd86
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-02-05 15:31:00 +0100

    Navegación y importo estilos de bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
 
-import './App.css'
+import { Navbar, Nav, Container } from "react-bootstrap";
+import {  BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import LoginPage from "./pages/loginPage";
+import CreateAccountPage from "./pages/CreateAccountPage";
+import HallPage from "./pages/HallPage";
+import ChatRoomPage from "./pages/ChatRoomPage";
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 function App() {
 
@@ -7,7 +15,33 @@ function App() {
   return (
     <>
 
-      <h1>Chat Sincrotron</h1>
+       
+    <Router>
+      <Container className="mt-3">
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand as={Link} to="/">Chat - sincrotron</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/login">Login</Nav.Link>
+              <Nav.Link as={Link} to="/register">Create Account</Nav.Link>
+              <Nav.Link as={Link} to="/hall">Hall</Nav.Link>
+              <Nav.Link as={Link} to="/chat-room">Chat room</Nav.Link>
+              
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <div className="mt-3">
+          <Routes>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<CreateAccountPage />} />
+            <Route path="/hall" element={<HallPage />} />
+            <Route path="/chat-room" element={<ChatRoomPage />} />
+            <Route path="/" element={<h2>Escoge una opción</h2>} />
+          </Routes>
+        </div>
+      </Container>
+    </Router>
  
     </>
   )
---
+++ src/pages/ChatRoomPage.tsx
@@ -0,0 +1,5 @@
+function ChatRoomPage() {
+    return ( <><h1>Chat room</h1></> );
+}
+
+export default ChatRoomPage;
