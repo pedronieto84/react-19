dd5aae0ce5f778b060a9672dff05d705bc9d62b9
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-02-05 20:17:09 +0100

    Aplico lógica que va actualizando el charts en tiempo real conforme van chateando los usuarios.

--- src/components/ChartComponent.tsx
+++ src/components/ChartComponent.tsx
@@ -1,5 +1,68 @@
-function ChartComponent() {
-    return ( <></> );
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Filler,
+    Legend,
+  } from 'chart.js';
+  import { Line } from 'react-chartjs-2';
+import { doc } from 'firebase/firestore';
+
+function ChartComponent(props) {
+    function generarArrayIncremental(array) {
+        // Crear un nuevo array con la misma longitud que el array de entrada
+        return Array.from({ length: array.length }, (_, index) => index + 1);
+      }
+
+    ChartJS.register(
+        CategoryScale,
+        LinearScale,
+        PointElement,
+        LineElement,
+        Title,
+        Tooltip,
+        Filler,
+        Legend
+      );
+
+    console.log(props.docData);
+
+
+    const options = {
+        responsive: true,
+       
+      };
+      
+      // Etiquetas para el eje X
+      
+       const labels = generarArrayIncremental(props.docData);
+      
+       const data = {
+        labels,
+        datasets: [
+          {
+            fill: true, // Si rellena o no el area de abajo
+            label: 'Dataset 2', // Nombre del set
+            data: props.docData, // Datos
+            borderColor: 'rgb(53, 162, 235)', // esto lo puedo quitar y escoge otro color por default
+            backgroundColor: 'rgba(53, 162, 235, 0.5)', // tambien puedo quitarlo o cambiarlo
+          },
+        ],
+      };
+   
+    return ( 
+    <>
+    <h1>ChartComponent</h1>
+   
+    <>
+       <Line options={options} data={data} />; 
+      </>
+    
+    </> );
 }
 
 export default ChartComponent;
--- src/pages/ChartPage.tsx
+++ src/pages/ChartPage.tsx
@@ -1,45 +1,53 @@
-import  { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import ChartComponent from '../components/ChartComponent';
 import { doc, onSnapshot } from "firebase/firestore";
 import { db } from "./../hooks/firebaseConfig";
-function ChartPage() {
 
+function ChartPage() {
     console.log('chartPage');
     const [docData, setDocData] = useState(null);
     const [loading, setLoading] = useState(true);
 
+    const [arrayTotalEscrito, setArrayTotalEscrito] = useState([]);
+
     // The ID of the document you want to listen to
     const docId = "user123"; // Replace with your document's ID
-  
-    useEffect(() => {
-      // Reference to the document in Firestore
-      const docRef = doc(db, "stats", 'globalStats');
-  
-      // Set up the listener
-      const unsubscribe = onSnapshot(docRef, (docSnap) => {
-        if (docSnap.exists()) {
-          // When the document data changes, update the state
-          const data = docSnap.data();
-          console.log('changes', data);
-          setDocData(data);
-        } else {
-          console.log("No such document!");
-        }
-      });
-  
-      // Cleanup the listener when the component is unmounted
-      return () => unsubscribe();
-    }, [docId]);
 
+    useEffect(() => {
+        // Reference to the document in Firestore
+        const docRef = doc(db, "stats", 'globalStats');
+
+        // Set up the listener
+        const unsubscribe = onSnapshot(docRef, (docSnap) => {
+            if (docSnap.exists()) {
+                // When the document data changes, update the state
+                const data = docSnap.data();
+                console.log('changes', data.length);
+                const length = data.length;
+                setLoading(false);
+                setDocData(data);
+                
+                // Correctly update the state of arrayTotalEscrito
+                setArrayTotalEscrito((prevArray) => [...prevArray, length]);
+
+                console.log('arrayTotalEscrito', arrayTotalEscrito);
+                
+            } else {
+                console.log("No such document!");
+            }
+        });
+
+        // Cleanup the listener when the component is unmounted
+        return () => unsubscribe();
+    }, []); // Dependencia de reload para manejar la actualización
 
     if (loading) return <div className='card'>Cargando...</div>;
 
-    return (<>
-
-
-        <ChartComponent />
-
-    </>);
+    return (
+        <>
+            <ChartComponent docData={arrayTotalEscrito} />
+        </>
+    );
 }
 
 export default ChartPage;
