173f839db30f3e14ba0a649826913afd1462ad5c
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-02-05 15:51:53 +0100

    Pongo el snippet de Firebase en app.tsx

--- src/pages/loginPage.tsx
+++ src/pages/loginPage.tsx
@@ -9,6 +9,12 @@ function LoginPage() {
     return (
         <div>
             <LoginFormComponent formSubmitted={loginFormData} />
+            <div className="container bg-light">
+            <div className="col-md-12 text-center mt-20">
+            <button type="button" className="btn btn-primary">Submit</button>
+            <button type="button" className="btn btn-warning">Cancel</button>
+        </div>
+</div>
         </div>);
 }
 
