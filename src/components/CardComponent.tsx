03080119d7beb1449e3be1826f8068a658c992ac
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-02-05 01:10:19 +0100

    Ejemplo de carga de Imagen desde dentro de cada Componente.

--- src/components/CardComponent.tsx
+++ src/components/CardComponent.tsx
@@ -1,14 +1,54 @@
 
 import { Card } from "react-bootstrap";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-const CardComponent = ({pokemon}) => {
+const CardComponent = ({user}) => {
 
-    
+  console.log('props user', user);
+
+  // Este componente va a ser un componente con estado
+  // Tengo el problema que no tengo la url de la imagen
+
+     // Data state donde almacenaré la response de la api
+      const [data, setData] = useState(null);
+  
+       // Para gestionar el estado de loading y error
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+      const fetchPoke = async () => {
+        try {
+          console.log('inicio fetch');
+          // Simulamos un retraso de 3 segundos
+          await new Promise((resolve) => setTimeout(resolve, 3000));
+  
+          const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${user.name}`);
+          if (!response.ok) {
+            throw new Error('No se pudo obtener la información de Pikachu');
+          }
+          const data = await response.json();
+          setData(data); // Guardamos los datos en el estado
+        } catch (error) {
+          setError(error.message); // Guardamos el error en el estado
+        } finally {
+          setLoading(false); // Indicamos que la carga ha terminado
+        }
+      };
+  
+      fetchPoke(); // Llamamos a la función
+    },[]); //
+
+ 
+  console.log('user', user)
+  if (loading) return <div className="card">Cargando...</div>;
+  if (error) return <div className="card">{error}</div>;
   return (
     <Card className="h-100 shadow-sm">
-      <Card.Img variant="top" src={pokemon.photo} alt={pokemon.name} />
+      {/* <Card.Img variant="top" src={user.sprites.front_default}  /> */}
       <Card.Body>
-        <Card.Title>{pokemon.name}</Card.Title>
+        <Card.Title>{user.name}</Card.Title>
         
       </Card.Body>
     </Card>
--- src/pages/CardsPage.tsx
+++ src/pages/CardsPage.tsx
@@ -1,11 +1,53 @@
 import CardComponent from "./../components/CardComponent"
-
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { FlexibleObjectTable } from "../types/types";
+import { Container, Row, Col } from "react-bootstrap";
 function FormsPage(){
+
+    // Data state donde almacenaré la response de la api
+    const [data, setData] = useState<FlexibleObjectTable[]>([]);
+
+     // Para gestionar el estado de loading y error
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  // Fetch data from the Pokémon API
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://pokeapi.co/api/v2/pokemon?limit=10"
+        );
+        setData(response.data.results); // Store the list of Pokémon
+        setLoading(false); // Set loading to false
+      } catch (error) {
+        const errorString = new Error(String(error)); // Convertir el error a un objeto `Error`
+        console.error(error);
+        setError(errorString.message); // Handle errors
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+  if (loading) return <div className="card">Cargando...</div>;
+  if (error) return <div className="card">{error}</div>;
 
     return(
         <>
         <h1 className='card'>Cards</h1>
-        <CardComponent/>
+        <Container className="mt-4">
+      <Row>
+        {data.map((user) => (
+          <Col key={user.id} xs={12} sm={6} md={4} lg={3} className="mb-4">
+            <CardComponent user={user}/>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+
+        
         </>
     )
 }
