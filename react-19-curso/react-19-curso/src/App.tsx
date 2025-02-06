df5ca5c23aacfcbab3daa412f5135035ea45b25b
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-02-06 01:32:56 +0100

    Añado navegación para testear el unsubscribe

--- react-19-curso/react-19-curso/src/App.tsx
+++ react-19-curso/react-19-curso/src/App.tsx
@@ -1,10 +1,11 @@
-
 import './App.css'
 import Counter from './components/Counter'
 import PostList from './components/PostsList'
 import { Provider } from "react-redux";
 import store from './store/store';
 import React from "react";
+
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
 
@@ -13,8 +14,26 @@ function App() {
     <>
       <React.StrictMode>
         <Provider store={store}>
-          <Counter />
-          <PostList />
+        <Router>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/counter">Counter</Link>
+              </li>
+              <li>
+                <Link to="/posts">Posts</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path="/" element={<PostList />} />
+            <Route path="/counter" element={<Counter />} />
+            <Route path="/posts" element={<PostList />} />
+          </Routes>
+        </Router>
         </Provider>
       </React.StrictMode>
     </>
