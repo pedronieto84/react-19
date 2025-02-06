83f25207df5e6edc02cf82ca4e75733172fedd9c
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-02-06 01:55:54 +0100

    NO REVISADO BIEN. Ejemplo de como ocultar rutas si no estoy logueado.

--- react-19-curso/react-19-curso/src/App.tsx
+++ react-19-curso/react-19-curso/src/App.tsx
@@ -4,8 +4,14 @@ import PostList from './components/PostsList'
 import { Provider } from "react-redux";
 import store from './store/store';
 import React from "react";
+import { useSelector } from 'react-redux';
 
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+const ProtectedRoute = ({ element }) => {
+  const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+  return isAuthenticated ? element : <div>You need to log in to view this page.</div>;
+};
 
 function App() {
 
@@ -31,7 +37,7 @@ function App() {
           <Routes>
             <Route path="/" element={<PostList />} />
             <Route path="/counter" element={<Counter />} />
-            <Route path="/posts" element={<PostList />} />
+            <Route path="/posts" element={<ProtectedRoute element={<PostList />} />} />
           </Routes>
         </Router>
         </Provider>
--- react-19-curso/react-19-curso/src/store/store.ts
+++ react-19-curso/react-19-curso/src/store/store.ts
@@ -1,18 +1,26 @@
 // src/store/store.js
 
-import { applyMiddleware, createStore, compose  } from "redux";
+import { applyMiddleware, createStore, compose, combineReducers } from "redux";
 import counterReducer from "./reducers";
+import authReducer from "./reducers/authReducer";
 import { thunk } from "redux-thunk";
-
 
 // Enable Redux DevTools Extension if available
 const composeEnhancers =
-  (typeof window !== 'undefined' && (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+  (typeof window !== "undefined" &&
+    (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+// Combine reducers
+const rootReducer = combineReducers({
+  counter: counterReducer,
+  auth: authReducer,
+});
 
 // Combine middleware and enhancers
 const enhancer = composeEnhancers(applyMiddleware(thunk)); // Add other middleware if needed
 
 // Create the store
-const store = createStore(counterReducer, enhancer);
+const store = createStore(rootReducer, enhancer);
 
 export default store;
